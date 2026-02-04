396e7dd8f9b4d282d1bf263fb6bddda94dfb8797
KernelBOSS-SYAM <filippo.frassinelli.25@stud.itsaltoadriatico.it>
2026-02-04 20:49:20 +0100

    Inizio griglia

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-
 # Inizializzazione
 pygame.init()
 screen = pygame.display.set_mode((900, 600))
@@ -8,15 +7,32 @@ screen = pygame.display.set_mode((900, 600))
 auto = pygame.image.load('./img/main.png').convert()
 auto = pygame.transform.scale(auto,(450,600))
 
+grid = [[-1 for _ in range(13)] for _ in range(13)]
+print(grid)
+
+CELL_SIZE = 30
+ROWS = 13
+COLS = 13
+
+WIDTH = COLS * CELL_SIZE
+HEIGHT = ROWS * CELL_SIZE
+
+def draw_grid():
+    for row in range(ROWS):
+        for col in range(COLS):
+            rect = pygame.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+            pygame.draw.rect(screen, (0, 0, 255), rect, 1)  # bordo blu
+
 # Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # 2. Disegnare lo sfondo (alle coordinate 0,0)
+            
     screen.blit(auto, (0, 0))
+    draw_grid()
+
 
     # Aggiornare lo schermo
     pygame.display.flip()
---
+++ test.py
@@ -0,0 +1,3 @@
+grid = [[-1 for _ in range(13)] for _ in range(13)]
+
+print(grid)

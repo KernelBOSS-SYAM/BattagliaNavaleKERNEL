4537447ee3c82f6aa45d6e281eddacb0aef16c7c
zFedericoz <federico.venturuzzo.25@stud.itsaltoadriatico.it>
2026-02-04 16:38:52 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+
+# Inizializzazione
+pygame.init()
+screen = pygame.display.set_mode((900, 600))
+
+# 1. Caricare l'immagine di sfondo
+auto = pygame.image.load('Desktop/main.png').convert()
+auto = pygame.transform.scale(auto,(450,600))
+rotated_image = pygame.transform.rotate(auto, -90)
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # 2. Disegnare lo sfondo (alle coordinate 0,0)
+    screen.blit(auto, (0, 0))
+
+    # Aggiornare lo schermo
+    pygame.display.flip()
+
+pygame.quit()

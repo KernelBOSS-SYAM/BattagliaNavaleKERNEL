435a62a189bc60f3586d008aef0f0e57295f99bd
KernelBOSS-SYAM <filippo.frassinelli.25@stud.itsaltoadriatico.it>
2026-02-05 15:17:46 +0100

    prova1

--- main.py
+++ main.py
@@ -1,37 +1,58 @@
 import pygame
+#COSTANTI
+CELL_SIZE = 30
+WIDTH = 13 * CELL_SIZE
+HEIGHT = 13 * CELL_SIZE
+
+
 # Inizializzazione
 pygame.init()
-screen = pygame.display.set_mode((900, 600))
+screen = pygame.display.set_mode((1500, 800))
 
 # 1. Caricare l'immagine di sfondo
-auto = pygame.image.load('./img/main.png').convert()
-auto = pygame.transform.scale(auto,(450,600))
-
+img_sfondo = pygame.image.load('./img/main.png').convert()
+img_sfondo = pygame.transform.scale(img_sfondo,(500,700))
+screen.blit(img_sfondo, (0, 0))
 grid = [[-1 for _ in range(13)] for _ in range(13)]
+pygame.display.set_caption("Battaglia Navale")
 print(grid)
 
-CELL_SIZE = 30
-ROWS = 13
-COLS = 13
 
-WIDTH = COLS * CELL_SIZE
-HEIGHT = ROWS * CELL_SIZE
 
 def draw_grid():
-    for row in range(ROWS):
-        for col in range(COLS):
+    for row in range(13):
+        for col in range(13):
             rect = pygame.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
             pygame.draw.rect(screen, (0, 0, 255), rect, 1)  # bordo blu
+
+draw_grid()
+imgC = pygame.image.load('./img/Corazzata.png')
+imgC = pygame.transform.scale(imgC, (150, 150))
+
+rectC = pygame.Rect(30, 30, 100, 100)
+rectC.bottomleft = (30, 200)
+screen.blit(imgC, rectC)
+
+
+
 
 # Game loop
 running = True
 while running:
+
+    
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            col = x // CELL_SIZE
+            row = y // CELL_SIZE
             
-    screen.blit(auto, (0, 0))
-    draw_grid()
+            print("Cella:", row, col)
+        
 
 
     # Aggiornare lo schermo
